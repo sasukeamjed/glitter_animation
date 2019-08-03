68ba3fff8ffe54cc5772b2e029f11b465b90f3d1
sasukeamjed <sasukeamjed@gmail.com>
2019-08-03 22:30:15 +0400

    added the animated glitter pieces

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
-final _width = 300.0;
-final _height = 300.0;
+const _width = 300.0;
+const _height = 300.0;
+const _duration = Duration(milliseconds: 500);
+final _colorTween = ColorTween(begin: Colors.green, end: Colors.transparent);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,9 +31,59 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
 
-  Widget _createGlitterPiece(){
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: _duration,
+    );
+    _animation= _colorTween.animate(_controller);
+    _controller.addStatusListener((AnimationStatus status){
+      if(status == AnimationStatus.completed){
+        _controller.reverse();
+      }else if(status == AnimationStatus.dismissed){
+        _controller.forward();
+      }
+    });
+    _controller.forward();
+  }
+
+
+//  @override
+//  void didUpdateWidget(MyHomePage oldWidget) {
+//    super.didUpdateWidget(oldWidget);
+//    _controller.duration = _duration;
+//  }
+
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _createAnimatedGlitterPiece(){
+    final random = math.Random();
+    final pieceSize = 10.0;
+    final piece = AnimatedBuilder(animation: _animation, builder: (BuildContext context, Widget widget){
+      return Container(width: pieceSize, height: pieceSize, color: _animation.value,);
+    });
+    final topOffSet = random.nextDouble() * (_width - pieceSize);
+    final leftOffSet = random.nextDouble() * (_height - pieceSize);
+    return Positioned(
+      top: topOffSet,
+      left: leftOffSet,
+      child: piece,
+    );
+  }
+
+  Widget _createStaticGlitterPiece(){
     final random = math.Random();
     final pieceSize = 10.0;
     final piece = Container(width: pieceSize, height: pieceSize, color: Colors.blue,);
@@ -47,7 +99,10 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Widget> _createGlitterPieces(int numPieces){
     final pieces = <Widget>[];
     for(var i = 0; i < numPieces; i++){
-      pieces.add(_createGlitterPiece());
+      pieces.add(_createStaticGlitterPiece());
+    }
+    for(var i = 0; i < numPieces; i++){
+      pieces.add(_createAnimatedGlitterPiece());
     }
     return pieces;
   }
@@ -57,7 +112,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Center(
